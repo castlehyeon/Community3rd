376a8b9aebe9df1acc34f92b078c58a32342d835
castlehyeon <qnftktls118@naver.com>
2022-08-02 10:41:35 +0900

    anotation정의

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -1,14 +1,30 @@
+// AppTest.java
 package com.ll.exam;
 
+import com.ll.exam.article.controller.ArticleController;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
     @Test
-    public void junit_assertThat(){
+    public void junit_assertThat() {
         int rs = 10 + 20;
 
         assertThat(rs).isEqualTo(30);
+    }
+
+    @Test
+    public void ioc__articleController() {
+        ArticleController articleController = Container.getArticleController();
+
+        assertThat(articleController).isNotNull();
+    }
+    @Test
+    public ioc__articleContainer__싱글톤(){
+        ArticleController articleController1 = Container.getArticleController();
+        ArticleController articleController2 = Container.getArticleController();
+
+        assertThat(articleController2)
     }
 }
